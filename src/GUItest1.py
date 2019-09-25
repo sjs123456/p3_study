1b7729e5686fd5f55ecdc32568746ba0d24a47b6
ex-shijs001 <ahusjs@163.com>
2019-09-25 15:19:08 +0800

    add pywinauto test

---
+++ src/GUItest1.py
@@ -0,0 +1,14 @@
+#_*_coding=utf-8_*_
+
+import pywinauto
+from subprocess import Popen
+from pywinauto import Desktop
+from pywinauto import mouse
+from pywinauto import keyboard
+import time
+
+app = pywinauto.Application().start('D:\\Users\ex-shijs001\AppData\Local\Programs\coco_test\coco_test.exe')
+main_title = 'coco_test'
+app[main_title].print_control_identifiers()
+time.sleep(5)
+app.coco_test.close()
